cfb3c996659970c45e49d46dac3f0437c29fa020
Amir A. Shabani <amirshabani011@gmail.com>
2020-03-20 12:58:41 +0330

    Change font to Sahel

--- exercise/2nd/index.php
+++ exercise/2nd/index.php
@@ -1,50 +1,56 @@
 <!DOCTYPE html>
 <html dir="rtl">
-<form action="form.php" method="POST">
-	نام خانوادگی: <input name="name" type="text"> <br> <br>
-	میزان کارکرد: <input name="time" type="text"> <br> <br>
-	<br>
+	<head>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="font-face.css"/>
+	</head>
+	<body>
+		<form action="form.php" method="POST">
+			نام و نام خانوادگی: <input name="name" type="text"> <br> <br>
+			میزان کارکرد: <input name="time" type="text"> <br> <br>
+			<br>
 
-	جنسیت:
-	زن<input name="gender" type="radio" value="f" checked>
-	مرد<input name="gender" type="radio" value="m"> <br> <br>
-	<br>
+			جنسیت:
+			زن<input name="gender" type="radio" value="f" checked>
+			مرد<input name="gender" type="radio" value="m"> <br> <br>
+			<br>
 
-	مدرک تحصیلی:
-	لیسانس<input name="degree" type="radio" value="bachelor" checked>
-	فوق لیسانس<input name="degree" type="radio" value="master">
-	دکترا<input name="degree" type="radio" value="doctor">
-	فوق دکترا<input name="degree" type="radio" value="postdoc"> <br> <br>
-	<br>
+			مدرک تحصیلی:
+			لیسانس<input name="degree" type="radio" value="bachelor" checked>
+			فوق لیسانس<input name="degree" type="radio" value="master">
+			دکترا<input name="degree" type="radio" value="doctor">
+			فوق دکترا<input name="degree" type="radio" value="postdoc"> <br> <br>
+			<br>
 
-	شرایط: <br>
-	بدی اب و هوا<input name="weather" type="checkbox"> <br>
-	سختی کار<input name="work" type="checkbox"> <br>
-	حق عائله‌مندی<input name="family" type="checkbox"> <br> <br>
-	<br>
+			شرایط: <br>
+			بدی اب و هوا<input name="weather" type="checkbox"> <br>
+			سختی کار<input name="work" type="checkbox"> <br>
+			حق عائله‌مندی<input name="family" type="checkbox"> <br> <br>
+			<br>
 
-	شهر محل فعالیت:
-	<select name="city">
-		<option value="tehran">تهران</option>
-		<option value="qom">قم</option>
-		<option value="shiraz">شیراز</option>
-		<option value="mashhad">مشهد</option>
-		<option value="kordestan">کردستان</option>
-		<option value="gilan">گیلان</option>
-		<option value="tabriz">تبریز</option>
-		<option value="ardebil">اردبیل</option>
-		<option value="semnan">سمنان</option>
-		<option value="bandarabas">بندرعباس</option>
-		<option value="sistan">سیستان</option>
-		<option value="zahedan">زاهدان</option>
-		<option value="mazandaran">مازندران</option>
-	</select> <br>
-	<br>
+			شهر محل فعالیت:
+			<select name="city">
+				<option value="tehran">تهران</option>
+				<option value="qom">قم</option>
+				<option value="shiraz">شیراز</option>
+				<option value="mashhad">مشهد</option>
+				<option value="kordestan">کردستان</option>
+				<option value="gilan">گیلان</option>
+				<option value="tabriz">تبریز</option>
+				<option value="ardebil">اردبیل</option>
+				<option value="semnan">سمنان</option>
+				<option value="bandarabas">بندرعباس</option>
+				<option value="sistan">سیستان</option>
+				<option value="zahedan">زاهدان</option>
+				<option value="mazandaran">مازندران</option>
+			</select> <br>
+			<br>
 
-	مدارک تحصیلی: 
-	<input name="file" type="file">
-	<input name="submit" type="submit" value="دخیره‌سازی">
-</form>
+			مدارک تحصیلی: 
+			<input name="file" type="file">
+			<input name="submit" type="submit" value="دخیره‌سازی">
+		</form>
+	</body>
 </html>
 
 <?php include_once("script.php"); ?>
